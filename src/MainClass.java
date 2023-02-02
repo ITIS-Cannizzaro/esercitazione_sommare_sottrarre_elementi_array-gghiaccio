5aada01ed3c5d4ba0bc7f5c8e0abef7945133f42
unknown user <giaada.donizetti@gmail.com>
2023-02-02 10:25:40 +0100

    consegna

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,43 +1,147 @@
 import java.util.Scanner;
 
-// Chiamare la classe col proprio cognome
+
 public class MainClass
 {
 	static Scanner in = new Scanner(System.in);
 	
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		while(true)
+		Scanner in = new Scanner(System.in);
+		System.out.println("Benvenuto, effettuare una scelta");
+		System.out.println("--------------------------------");
+		System.out.println("1. somma indici pari");
+		System.out.println("2. differenza indici dispari ");
+		System.out.println("3. inverso ");
+		System.out.println("4. inverso stringa ");
+		System.out.println("--------------------------------");
+		int Scelta = in.nextInt();
+		
+	
+		
+		switch(Scelta)
 		{
-			stampaMenu();
-			int scelta = Integer.parseInt(in.nextLine());
-			switch(scelta)
-			{
-				//aggiungere, togliere casi a seconda delle proprie scelte
-				case 1:
-					//Inserire metodo statico
+			case 1 : 
+				Pari();
 				break;
-				case 2:
-					//Inserire metodo statico
+			
+			case 2:
+				Dispari();
 				break;
-				case 3:
-					//Inserire metodo statico
+			
+			case 3:
+				Inverso();
 				break;
-				default:
-					System.out.println("Scelta errata, riprova!");
-
-			}
-		}
+			case 4:
+				InversoStrng();
+				break;
+			default:
+				Def();
+				break;
+		}		
 	}
-	static void stampaMenu()
+	public static void Def()
 	{
-		// Modificare il menù secondo le proprie scelte
-		System.out.println("1 - Es n. * - Titolo es. *");
-		System.out.println("2 - Es n. * - Titolo es. *");
-		System.out.println("3 - Es n. * - Titolo es. *");
-		System.out.println("4 - Es n. * - Titolo es. *");
-		System.out.println("5 - Es n. * - Titolo es. *");
+		System.out.println("Non esiste alcuna scelta");
 	}
 	
-	// Aggiungere qui i metodi statici relativi agli esercizi scelti
+	public static void Pari()
+	{
+		int Somma = 0;
+		int Numeri[] = new int [6];
+		for( int i = 0; i<Numeri.length; i++)
+		{
+			System.out.println("Inserire un numero");
+			Numeri[i]=in.nextInt();
+		}
+		for(int i=0; i < Numeri.length; i+=2)
+		{
+			Somma+=Numeri[i];
+		}
+		System.out.println("La somma è pari a " + Somma);
+	}
+	
+	public static void Dispari()
+	{
+		int Differenza = 0;
+		int Numeri[] = new int [6];
+		for( int i = 0; i<Numeri.length; i++)
+		{
+			System.out.println("Inserire un numero");
+			Numeri[i]=in.nextInt();
+		}
+		for(int i=1; i<Numeri.length; i+=2)
+		{
+			Differenza-=Numeri[i];
+		}
+		System.out.println("La differenza è " + Differenza);
+		
+	}
+	
+	public static void Inverso()
+	{
+		int Numeri[] = new int [6];
+		for( int i = 0; i<Numeri.length; i++)
+		{
+			System.out.println("Inserire un numero");
+			Numeri[i]=in.nextInt();
+		}
+		
+		int NumeriInversi[] = new int [Numeri.length];
+		for (int i = Numeri.length; i<=0;i--)
+		{
+			int Contatore = 0;
+			Contatore ++;
+			NumeriInversi[Contatore] = Numeri[i];
+		}
+		
+		
+		for(int i=0; i<Numeri.length; i++)
+		{
+			System.out.println( NumeriInversi[i]);
+		}
+		
+	}
+	
+	public static void InversoStrng()
+	{
+		String Stringa[] = new String [6];
+		for( int i = 0; i<Stringa.length; i++)
+		{
+			System.out.println("Inserire un numero");
+			Stringa[i]=in.nextLine();
+		}
+		
+		int Stringaa[] = new int [Stringa.length];
+		for (int i = Stringa.length; i<=0;i--)
+		{
+			int Contatore = 0;
+			Contatore ++;
+			Stringaa[Contatore] = Stringa[i];
+		}
+		
+		
+		for(int i=0; i<Stringa.length; i++)
+		{
+			System.out.println( Stringaa[i]);
+		}
+	}
+	
+	public static void Length()
+	{
+		int Numeri[] = new int [6];
+		for( int i = 0; i<Numeri.length; i++)
+		{
+			System.out.println("Inserire un numero");
+			Numeri[i]=in.nextInt();
+		}
+		int Contatore = 0;
+		for(int i=0; i<Numeri.length; i++)
+		{
+			Contatore++;
+		}
+		System.out.println("La lunghezza equivale a " + Contatore);
+	}
 }
+	
+	
